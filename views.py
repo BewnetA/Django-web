75d6ce74b11d98eec30ada64b695cefb741b6893
unknown <bewnetaddisalem@gmail.com>
2024-09-07 11:27:46 +0300

    Django web

---
+++ views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import post 
+
+def home(request):
+     context = {
+          "posts": post.objects.all(),
+     }
+     return render(request, 'blog/home.html', context)
+def about(request):
+     return render(request, "blog/about.html",{"title": "About"})
